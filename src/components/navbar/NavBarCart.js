2a428618450a32d43f185a1c02d1ff455da33897
Rodrigo Lago <rodrigo.n.lago@gmail.com>
2021-11-13 13:56:47 -0300

    add tailwind css

---
+++ src/components/navbar/NavBarCart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavBarCart() {
+    return (
+        <div>
+            <h1>NAVBAR CART</h1>
+        </div>
+    )
+}
---
+++ src/containers/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavBarCart from '../components/navbar/NavBarCart'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <NavBarCart />
+      </div>
+    )
+  }
+}
+
+  
